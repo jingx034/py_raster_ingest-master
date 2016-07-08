68cf5c59b4b90e45cfd554a6d4b727eebd935ebb
jingx034 <jingx034@umn.edu>
2016-07-08 11:40:07 -0500

    build xml file

--- add_GeoServer_style.py
+++ add_GeoServer_style.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jun 22 10:08:57 2016
+Improve on Fri July 8 2016
 
 @author: jingx034
 
@@ -9,40 +10,41 @@ Build xml file
 
 import lxml.etree as ET
 import csv
+import ntpath
 
-# the input file path need to be change
-filepath = r'C:\Users\jingx034\Desktop\code\legend\Globcover2009_Legend.csv'
-outpath = r'C:\data\globcover\Globcover2009.xml'
+def build_xml(csv_path,xml_path,file_path):
+    path,file_name = ntpath.split(xml_path)
 
-
-Descriptor = ET.Element('StyledLayerDescriptor',version = "1.0.0",xmlns="http://www.opengis.net/sld" )
-
-named = ET.SubElement(Descriptor,'NamedLayer')
-ET.SubElement(named,'Name').text = "Globcover2009_legend"
-
-
-User = ET.SubElement(named,'UserStyle')
-ET.SubElement(User,'Title').text = "Global Land Cover 2009 Legend"
-ET.SubElement(User,'Abstract').text = "Wen fill in"
-
-feature = ET.SubElement(User,'FeatureTypeStyle')
-rule = ET.SubElement(feature,'Rule')
-raster = ET.SubElement(rule,'RasterSymbolizer')
-colormap = ET.SubElement(raster,'ColorMap',type = "intervals")
-
-
-with open(filepath,'rb') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:  
-       #This line for dynamic
-       entry = ET.SubElement(colormap,"ColorMapEntry",color = row['Color'],quantity = "%s" % row['Value'],opacity = "${env('%s',0)}"% row['mnemonics'])
-       
-       #This line for basic legend
-       #entry = ET.SubElement(colormap,"ColorMapEntry",color = row['Color'],quantity = "%s" % row['Value'])
+        
+    Descriptor = ET.Element('StyledLayerDescriptor',version = "1.0.0",xmlns="http://www.opengis.net/sld" )
+    
+    with open(file_path,'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader: 
+            if row ['filename'] == file_name[:-4]:
+                named = ET.SubElement(Descriptor,'NamedLayer')
+                ET.SubElement(named,'Name').text = row['name']
+                User = ET.SubElement(named,'UserStyle')
+                ET.SubElement(User,'Title').text = row['title']
+                ET.SubElement(User,'Abstract').text = row['abstrict']
+                feature = ET.SubElement(User,'FeatureTypeStyle')
+                rule = ET.SubElement(feature,'Rule')
+                raster = ET.SubElement(rule,'RasterSymbolizer')
+                colormap = ET.SubElement(raster,'ColorMap',type = row['map_type'])
 
 
+                with open(csv_path,'rb') as csvfile:
+                    read = csv.DictReader(csvfile)
+                    for classes in read:
+                        #This line for dynamic
+                        ET.SubElement(colormap,"ColorMapEntry",color = classes['Color'],quantity = "%s" % classes['Value'],opacity = "${env('%s',0)}"% classes['mnemonics'])
 
-tree = ET.ElementTree(Descriptor)
-tree.write(outpath,pretty_print=True,encoding = "utf-8",xml_declaration =True)
+   #This line for basic legend
+   #entry = ET.SubElement(colormap,"ColorMapEntry",color = classes['Color'],quantity = "%s" % classes['Value'])
+    tree = ET.ElementTree(Descriptor)
+    tree.write(xml_path,pretty_print=True,encoding = "utf-8",xml_declaration =True)
 
 
+#Make sure the the file name in xml_path is the same with filename in style_info.csv!!!
+build_xml('C:\Users\jingx034\Desktop\code\legend\Globcover2009_Legend.csv','C:\data\globcover\Globcover2009.xml','C:\data\globcover\style_info.csv')   
+  
